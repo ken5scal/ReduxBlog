98ce81fd9b9978e78b95b8b0895534066ea23d25
ken5scal <kengoscal@gmail.com>
2017-01-23 08:41:44 +0900

    Update

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -15,7 +15,8 @@ class PostsNew extends Component {
     return (
       <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create A New Post</h3>
-        <div className="form-group">
+
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label>Title</label>
           <input type="text" className="form-control" {...title}/>
           <div className="text-help">
@@ -23,7 +24,7 @@ class PostsNew extends Component {
           </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
           <label>Categories</label>
           <input type="text" className="form-control" {...categories}/>
             <div className="text-help">
@@ -31,7 +32,7 @@ class PostsNew extends Component {
             </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
           <label>Content</label>
           <textarea className="form-control" {...content}/>
             <div className="text-help">
