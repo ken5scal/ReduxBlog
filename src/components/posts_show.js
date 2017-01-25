46976abbf2fe08f1b8429d829a40737dbebbc818
ken5scal <kengoscal@gmail.com>
2017-01-25 23:59:39 +0900

    Fix problem

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
+
 class PostsShow extends Component {
   render() {
-
-    return <div> show post {this.props.params.id};
+    return <div> show post {this.props.params.id}</div>
   }
 }
 
