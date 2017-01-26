c40d042fa28162689d11bebe65fba78a5de97dec
ken5scal <kengoscal@gmail.com>
2017-01-26 09:38:11 +0900

    Fix typo

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -35,10 +35,24 @@ class PostsNew extends Component {
     });
   }
 
+  renderField(fieldConfig, field) {
+    const fieldHelper = this.props.fields[field];
+
+    return (
+      <div className={`form-group ${fieldHelper.touched && fieldHelper.invalid ? 'has-danger' : ''}`}>
+        <label>{fieldConfig.label}</label>
+        <fieldConfig.type type="text" className="form-control" {...fieldHelper}/>
+        <div className="text-help">
+          {fieldHelper.touched ? fieldHelper.error : ''}
+        </div>
+      </div>
+    )
+  }
+
   render() {
-    const { fields: {title, categories, content}, handleSubmit } = this.props;
+    const { handleSubmit } = this.props;
     // const title = this.props.fields.title;
-    console.log(title);
+    // console.log(title);
 
     // ...title
     // Destructuring of the object(title) and passes its properties into input
@@ -47,29 +61,7 @@ class PostsNew extends Component {
       <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <h3>Create A New Post</h3>
 
-        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
-          <label>Title</label>
-          <input type="text" className="form-control" {...title}/>
-          <div className="text-help">
-            {title.touched ? title.error : ''}
-          </div>
-        </div>
-
-        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
-          <label>Categories</label>
-          <input type="text" className="form-control" {...categories}/>
-            <div className="text-help">
-              {categories.touched ? categories.error : ''}
-            </div>
-        </div>
-
-        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
-          <label>Content</label>
-          <textarea className="form-control" {...content}/>
-            <div className="text-help">
-              {content.touched ? content.error : ''}
-            </div>
-        </div>
+        {_.map(FIELDS, this.renderField.bind(this))}
 
         <button type="submit" className="btn btn-primary">Submit</button>
         <Link to="/" className="btn btn-danger">Cancel</Link>
